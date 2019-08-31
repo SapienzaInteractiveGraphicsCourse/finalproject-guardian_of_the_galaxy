9419e6a2fb40febc611dc0d098666f9aba9dce91
gabro96 <gab.sella96@gmail.com>
2019-08-31 15:10:48 +0100

    first planets implementation. Added galaxy background to the scene and in the initial page.

--- index.js
+++ index.js
@@ -58,7 +58,200 @@ enemystarship = {
 }
 
 
+//
+//PLANETS
+//
+var pointLight;
+var segmenti_pianeta = 48;
+var pianeta1_dimensione=3;
+var pianeta2_dimensione=5;
+var pianeta3_dimensione=6;
+var pianeta4_dimensione=1;
+var pianeta5_dimensione=4;
+var pianeta6_dimensione=9;
 
+var texture_planets=["./images/1.jpg","./images/2.jpg","./images/3.jpg","./images/4.jpg","./images/5.jpg","./images/6.jpg","./images/7.jpg","./images/8.jpg","./images/9.jpg","./images/10.jpg","./images/11.jpg","./images/12.jpg",
+             "./images/13.jpg","./images/14.jpg","./images/15.jpg","./images/16.jpg"];
+var planets_dimensions=[1,3,5,7,9,4];
+
+function shuffle(myArray,myLen){
+    var array=myArray;
+    var len=myLen;
+    var index,temp;
+    while(len>0){
+        index=Math.floor(Math.random()*len);
+        len=len-1;
+        temp=array[len];
+        array[len]=array[index];
+        array[index]=temp;
+    }
+}
+shuffle(texture_planets,16);
+shuffle(planets_dimensions,6);
+
+var pianeta1_data = constructPlanetData(0.015, (0,0,20), "pianeta1", texture_planets[0], planets_dimensions[0], segmenti_pianeta);
+var pianeta2_data = constructPlanetData(0.015, (15,15,20), "pianeta2", texture_planets[1], planets_dimensions[1], segmenti_pianeta);
+var pianeta3_data = constructPlanetData(0.015, (15,15,20), "pianeta3", texture_planets[2], planets_dimensions[2], segmenti_pianeta);
+var pianeta4_data = constructPlanetData(0.015, (-15,-15,-20), "pianeta4", texture_planets[3], planets_dimensions[3], segmenti_pianeta);
+var pianeta5_data = constructPlanetData(0.015, (15,15,20), "pianeta5", texture_planets[4], planets_dimensions[4], segmenti_pianeta);
+var pianeta6_data = constructPlanetData(0.015, (0,0,0), "pianeta6", texture_planets[5], planets_dimensions[5], segmenti_pianeta);
+
+shuffle(texture_planets);
+shuffle(planets_dimensions);
+
+function rotazione_pianeta(pianeta, myData) {
+    if(myData.name=='pianeta1'){
+        pianeta.rotation.y += myData.rotationRate;
+        pianeta.rotation.x += (myData.rotationRate)/3;
+    }
+    if(myData.name=='pianeta2'){
+        pianeta.rotation.y += myData.rotationRate;
+        pianeta.rotation.x += (myData.rotationRate)/3;
+    }
+    if(myData.name=='pianeta3'){
+        pianeta.rotation.y += myData.rotationRate;
+        pianeta.rotation.x += (myData.rotationRate)/3;
+    }
+    if(myData.name=='pianeta4'){
+        pianeta.rotation.y += myData.rotationRate;
+        pianeta.rotation.x += (myData.rotationRate)/3;
+    }
+    if(myData.name=='pianeta5'){
+        pianeta.rotation.y += myData.rotationRate;
+        pianeta.rotation.x += (myData.rotationRate)/3;
+    }
+    if(myData.name=='pianeta6'){
+        pianeta.rotation.y += myData.rotationRate;
+        pianeta.rotation.x += (myData.rotationRate)/3;
+    }
+}
+
+
+function movimento_pianeta(pianeta,myData){
+    /*if(pianeta.position.x >=-40){
+        pianeta.position.x -= 0.2;
+        pianeta.position.z += 0.2;
+    }
+    else{
+        pianeta.position.x=0;
+        pianeta.position.z=0;
+    }*/
+    if(pianeta.position.z< camera.position.z){
+        pianeta.position.x -= 0.2;
+        pianeta.position.z += 0.2;
+    }
+    else{
+        scene.remove(pianeta);
+    }
+}
+
+function constructPlanetData(rotation_rate_tmp, distance_from_axis_tmp, name_tmp, texture_tmp, size_tmp, segments_tmp) {
+    return { rotationRate: rotation_rate_tmp, distanceFromAxis: distance_from_axis_tmp, name: name_tmp, texture: texture_tmp, size: size_tmp, segments: segments_tmp };
+}
+
+function getMaterial(type, color, texture_tmp) {
+    var materialOptions = {
+        color: color === undefined ? 'rgb(255, 255, 255)' : color,
+        map: texture_tmp === undefined ? null : texture_tmp
+    };
+
+    switch (type) {
+        case 'basic':
+            return new THREE.MeshBasicMaterial(materialOptions);
+        case 'lambert':
+            return new THREE.MeshLambertMaterial(materialOptions);
+        case 'phong':
+            return new THREE.MeshPhongMaterial(materialOptions);
+        case 'standard':
+            return new THREE.MeshStandardMaterial(materialOptions);
+        default:
+            return new THREE.MeshBasicMaterial(materialOptions);
+    }
+}
+
+function getSphere(material, size, segments) {
+    var geometry = new THREE.SphereGeometry(size, segments, segments);
+    var obj = new THREE.Mesh(geometry, material);
+    obj.castShadow = true;
+
+    return obj;
+}
+
+
+function loadTexturedPlanet(myData, x, y, z, myMaterialType) {
+    var myMaterial;
+    var passThisTexture;
+
+    if (myData.texture && myData.texture !== "") {
+        //passThisTexture = new THREE.ImageUtils.loadTexture(myData.texture);
+        THREE.ImageUtils.crossOrigin = '';
+        var passThisTexture = THREE.ImageUtils.loadTexture(myData.texture);
+    }
+    if (myMaterialType) {
+        myMaterial = getMaterial(myMaterialType, "rgb(255, 255, 255 )", passThisTexture);
+    } else {
+        myMaterial = getMaterial("lambert", "rgb(255, 255, 255 )", passThisTexture);
+    }
+
+    myMaterial.receiveShadow = true;
+    myMaterial.castShadow = true;
+    var pianeta = getSphere(myMaterial, myData.size, myData.segments);
+    pianeta.receiveShadow = true;
+    pianeta.name = myData.name;
+    scene.add(pianeta);
+    pianeta.position.set(x, y, z);
+
+    return pianeta;
+}
+
+function getPointLight(intensity, color) {
+    var light = new THREE.PointLight(color, intensity);
+    light.castShadow = true;
+
+    light.shadow.bias = 0.001;
+    light.shadow.mapSize.width = 2048;
+    light.shadow.mapSize.height = 2048;
+    return light;
+}
+
+
+function updatePlanet(renderer, scene, camera, controls) {
+    pointLight.position.copy([0,0,0]);
+    controls.updatePlanet();
+
+    //pianeta1 movimenti
+    //rotazione_pianeta(pianeta1,pianeta1_data);
+    //movimento_pianeta(pianeta1,pianeta1_data);
+
+    //pianeta2 movimenti
+    //rotazione_pianeta(pianeta2,pianeta2_data);
+    //movimento_pianeta(pianeta2,pianeta2_data);
+
+    //pianeta3 movimenti
+    //rotazione_pianeta(pianeta3,pianeta3_data);
+    //movimento_pianeta(pianeta3,pianeta3_data);
+
+    //pianeta4 movimenti
+    //rotazione_pianeta(pianeta4,pianeta4_data);
+    //movimento_pianeta(pianeta4,pianeta4_data);
+
+    //pianeta5 movimenti
+    //rotazione_pianeta(pianeta5,pianeta5_data);
+    //movimento_pianeta(pianeta5,pianeta5_data);
+
+    //pianeta6 movimenti
+    //rotazione_pianeta(pianeta6,pianeta6_data);
+    //movimento_pianeta(pianeta6,pianeta6_data);
+
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(function () {
+        updatePlanet(renderer, scene, camera, controls);
+    });
+}
+//
+//END PLANETS
+//
 
 
 var loadManager = new THREE.LoadingManager();
@@ -96,7 +289,6 @@ function loadModels(loadManager){
 	//----MODELS-----
 	if(!scene) {
 		scene = new THREE.Scene();
-		scene.background= new THREE.Color( 0xffffff );
 	}
 
 	var starship_loader = new THREE.OBJLoader(loadManager);
@@ -164,11 +356,9 @@ function loadModels(loadManager){
 }
 
 function initGame() {
-	
 		
 	if (!scene) {
 		scene = new THREE.Scene();
-		scene.background= new THREE.Color( 0xffffff );
 	}
 				
 				
@@ -179,7 +369,7 @@ function initGame() {
 	}
 	
 	if (!renderer) {
-		renderer = new THREE.WebGLRenderer( { antialias:true, powerPreference:"high-performance" } );
+		renderer = new THREE.WebGLRenderer( { antialias:true,alpha: true, powerPreference:"high-performance" } );
 		renderer.setPixelRatio( window.devicePixelRatio );
 		renderer.setSize( window.innerWidth, window.innerHeight );
 		container = document.getElementById( 'container' );
@@ -210,6 +400,16 @@ function initGame() {
 	animate();
 
 	window.addEventListener( 'resize', onWindowResize, false );
+
+	var pianeta1 = loadTexturedPlanet(pianeta1_data, pianeta1_data.distanceFromAxis, 0, 0);
+    //var pianeta2 = loadTexturedPlanet(pianeta2_data, pianeta2_data.distanceFromAxis, 0, 0);
+    //var pianeta3 = loadTexturedPlanet(pianeta3_data, pianeta3_data.distanceFromAxis, 0, 0);
+    //var pianeta4 = loadTexturedPlanet(pianeta4_data, pianeta4_data.distanceFromAxis, 0, 0);
+    //var pianeta5 = loadTexturedPlanet(pianeta5_data, pianeta5_data.distanceFromAxis, 0, 0);
+    //var pianeta6 = loadTexturedPlanet(pianeta6_data, pianeta6_data.distanceFromAxis, 0, 0);
+
+
+    update(renderer, scene, camera, controls);
 	
 }
 
